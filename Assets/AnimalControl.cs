ae39e09c0f65e7381c76d7f709cf9cb646a7c81e
Dharma <dgop998@aucklanduni.ac.nz>
2018-05-31 19:31:35 +1200

    Initial commit

---
+++ Assets/AnimalControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+public class Animal
+{
+    //List all the columns of your table here in the same format as below.
+    public string AnimalId { get; set; }
+    //  public string Manufacturer { get; set; }
+    public string Complaint { get; set; }
+    //   public string Segment { get; set; }
+}
